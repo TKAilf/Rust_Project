5165c79f3810ec026dda2552704c641e52f4e0c5
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-04-08 22:11:04 +0900

    fix gitattributes

---
+++ branches/target/debug/deps/branches-f30a64f6113a6211.d
@@ -0,0 +1,5 @@
+C:\Users\ktosh\Desktop\Rust_Project\branches\target\debug\deps\libbranches-f30a64f6113a6211.rmeta: src\main.rs
+
+C:\Users\ktosh\Desktop\Rust_Project\branches\target\debug\deps\branches-f30a64f6113a6211.d: src\main.rs
+
+src\main.rs:
---
+++ loops/target/debug/deps/loops-2430a3d65557f50e.d
@@ -0,0 +1,5 @@
+C:\Users\ktosh\Desktop\Rust_Project\loops\target\debug\deps\libloops-2430a3d65557f50e.rmeta: src\main.rs
+
+C:\Users\ktosh\Desktop\Rust_Project\loops\target\debug\deps\loops-2430a3d65557f50e.d: src\main.rs
+
+src\main.rs:
