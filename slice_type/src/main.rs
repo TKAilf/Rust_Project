344d11f6b15cc24f594bbbdd3f7f7a8c1c3d9f98
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-04-10 12:31:17 +0900

    Fix slice_type

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -1,19 +1,67 @@
 fn main() {
-    let mut s = String::from("hello world");
+    // let mut s = String::from("hello world");
 
+    // let word = first_word(&s);
+
+    // s.clear();
+
+    // println!("The first word is: {}", word);
+
+    let s = String::from("hello world");
+    let hello = &s[0..5];
+    // let hello = &s[..5]; // 0 から始まる場合は省略可能
+    let world = &s[6..11];
+    // let world = &s[6..]; // 11 までの場合は省略可能
+    let slice = &s[..];
     let word = first_word(&s);
+    println!("hello:{}, world:{}, slice: {}, word: {}", hello, world, slice, word);
 
-    s.clear(); // error!
+    let my_string = String::from("hello world");
+    let word2 = first_word_aprove(&my_string[..]);
+    let my_string_literal = "hello world";
+    let word3 = first_word_aprove(&my_string_literal[..]);
+    let word4 = first_word_aprove(my_string_literal);
+    println!("word2: {}, word3: {}, word4: {}", word2, word3, word4);
 }
 
-fn first_word(s: &String) -> usize {
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+
+//     s.len()
+// }
+
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
-    for (i, &item) in bytes.iter().eumerate() {
+    for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[..i];
         }
     }
 
-    s.len()
+    &s[..]
+}
+
+// 以下の関数シグニチャにすることで、&String と &str の両方を引数に取ることができる。
+// 仮にStrinに対して扱いたいときは、String全体のスライスを取ることで対応できる。
+// 例えば、&s[..] とすることで、String 全体のスライスを取得できる。
+// また、文字列リテラルに対して扱いたいときは、文字列リテラルのスライスを取ることで対応できる。
+// しかし、文字列リテラル全体を扱いたいときは、文字列リテラルそのものを引数に取ることで対応できる。
+// なぜなら、文字列リテラルはそれ自体がすでに文字列スライスであるためである。
+fn first_word_aprove(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
