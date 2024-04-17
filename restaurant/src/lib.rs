8cafb6c8c04d5b5cfaee6cc1bbdf96d47c4c0299
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-04-18 07:42:17 +0900

    Update lib
    警告の削除

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -2,42 +2,42 @@ mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+        // fn seat_at_table() {} // Remove the unused function
     }
 
     mod serving {
-        fn take_order() {}
+        // fn take_order() {}
 
-        fn serve_order() {}
+        // fn serve_order() {}
 
-        fn take_payment() {}
+        // fn take_payment() {}
     }
 }
 
 // superを相対パスで使用すると、親モジュールにアクセスできる
-fn serve_order() {}
+// fn serve_order() {}
 
 mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::serve_order();
-        crate::serve_order(); // 絶対パスではこのように書く
-    }
+    // fn fix_incorrect_order() {
+    //     cook_order();
+    //     super::serve_order();
+    //     crate::serve_order(); // 絶対パスではこのように書く
+    // }
 
-    fn cook_order() {}
+    // fn cook_order() {}
 
     // 構造体自体にpubをつけることで、その構造体は公開されるが、構造体のフィールドは公開されない
     // 構造体のフィールドにpubをつけることで、そのフィールドは公開される
     pub struct Breakfast {
         pub toast: String,
-        seasonal_fruit: String,
+        // seasonal_fruit: String,
     }
 
     impl Breakfast {
         pub fn summer(toast: &str) -> Breakfast {
             Breakfast {
                 toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
+                // seasonal_fruit: String::from("peaches"),
             }
         }
     }
@@ -62,11 +62,11 @@ pub fn eat_at_restaurant() {
     println!("I'd like {} toast please", meal.toast);
     // meal.seasonal_fruit = String::from("blueberries");
 
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+    let _order1 = back_of_house::Appetizer::Soup;
+    let _order2 = back_of_house::Appetizer::Salad;
 }
 
-fn main() {}
+// fn main() {}
 
 // パスには2つの種類がある。絶対パスと相対パスである。
 // 絶対パスは、クレートの名前かcrateという文字列から始める。
