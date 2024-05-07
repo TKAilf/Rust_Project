65cb9c7156bb8047810c9093c6cc4bc3085b9bd8
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-05-07 19:54:10 +0900

    create generic_types_traits_lifetimes

---
+++ generic_types_traits_lifetimes/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // 関数を抽出することで重複を取り除く
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result)
+}
+
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = number_list[0];
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
