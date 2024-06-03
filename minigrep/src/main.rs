ef5d25fa87018e37bec42a649c964ecb90116854
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-06-03 16:24:21 +0900

    minigrep update for seach fnction by TDD

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,7 +7,6 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
     // let (query, filename) = parse_config(&args);
     // let config = Config::new(&args);
@@ -16,9 +15,6 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
