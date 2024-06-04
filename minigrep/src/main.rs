49c2be93c0a015ccca6017dc333185c807ba70ba
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-06-04 16:41:09 +0900

    Update minigrep for errormessage

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -14,17 +14,17 @@ fn main() {
     // let config = Config::new(&args);
     // Result型を返すようにリファクタリングしたことによる対応
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 
     // run(config).unwrap_or_else(|err| {
-    //     println!("Application error: {}", err);
+    //     eprintln!("Application error: {}", err);
     //     process::exit(1);
     // })
 }
