d885f6af82b615432963039a6fc8fb707925550f
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-06-25 00:26:12 +0900

    Update main.rs

--- smartpointer_tutorial/src/main.rs
+++ smartpointer_tutorial/src/main.rs
@@ -1,4 +1,5 @@
 use List::{Cons, Nil};
+use std::ops::Deref;
 
 fn main() {
     let b = Box::new(5);
@@ -8,9 +9,63 @@ fn main() {
                 Box::new(Cons(2,
                     Box::new(Cons(3,
                         Box::new(Nil))))));
+
+    print_list(&list);
+
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
 
 enum List {
     Cons( i32, Box<List> ),
     Nil,
+}
+
+fn print_list(list: &List) {
+    match list {
+        Cons(head, tail) => {
+            println!("{}", head);
+            print_list(tail);
+        }
+        Nil => {
+            println!("End of list");
+        }
+    }
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T>{
+    fn new(x: T) -> MyBox<T>{
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T>{
+    type Target = T;
+    fn deref(&self) -> &T{
+        &self.0
+    }
+}
+
+fn hello(name: &str){
+    println!("Hello {}!", name);
 }
