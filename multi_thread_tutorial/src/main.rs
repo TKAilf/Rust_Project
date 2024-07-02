3a31777c9ccd123befe4675dc7acddd990cbac16
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-07-02 21:49:47 +0900

    Update main.rs

--- multi_thread_tutorial/src/main.rs
+++ multi_thread_tutorial/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
     let handle = thread::spawn(|| {
@@ -30,4 +31,64 @@ fn main() {
     handle.join().unwrap();
 
     handle2.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![String::from("hi"), 
+                                     String::from("from"), 
+                                     String::from("the"), 
+                                     String::from("thread"),];
+        
+        for val in vals{
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for received in rx{
+        println!("Got: {}", received);
+    }
+
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let vals = vec![String::from("hi"), 
+                                     String::from("from"), 
+                                     String::from("the"), 
+                                     String::from("thread"),];
+        
+        for val in vals{
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![String::from("more"), 
+                                     String::from("messages"), 
+                                     String::from("for"), 
+                                     String::from("you"),];
+        
+        for val in vals{
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    for received in rx{
+        println!("Got: {}", received);
+    }
 }
