6670c5eb34584758ec4d5702ae7853e310cadcc1
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-07-06 18:09:45 +0900

    Update file

--- oop_paradigm_in_rust/src/main.rs
+++ oop_paradigm_in_rust/src/main.rs
@@ -1,3 +1,37 @@
+use oop_paradigm_in_rust::gui::Draw;
+use oop_paradigm_in_rust::gui::{Button, Screen};
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("Drawing a SelectBox: width: {}, height: {}, options: {:?}", self.width, self.height, self.options);
+    }
 }
