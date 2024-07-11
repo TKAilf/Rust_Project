00cef381990b9a5252e050d41026356db6d37183
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-07-11 23:53:39 +0900

    Update lib.rs

--- oop_paradigm_in_rust/src/lib.rs
+++ oop_paradigm_in_rust/src/lib.rs
@@ -79,18 +79,25 @@ pub mod blog {
         }
 
         pub fn content(&self) -> &str {
-            ""
+            self.state.as_ref().unwrap().content(&self)
         }
-        
+
         pub fn request_review(&mut self) {
             if let Some(s) = self.state.take() {
                 self.state = Some(s.request_review())
+            }
+        }
+
+        pub fn approve(&mut self) {
+            if let Some(s) = self.state.take() {
+                self.state = Some(s.approve())
             }
         }
     }
 
     trait State {
         fn request_review(self: Box<Self>) -> Box<dyn State>;
+        fn approve(self: Box<Self>) -> Box<dyn State>;
     }
 
     struct Draft {}
@@ -99,12 +106,32 @@ pub mod blog {
         fn request_review(self: Box<Self>) -> Box<dyn State> {
             Box::new(PendingReview {})
         }
+
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
     }
 
     struct PendingReview {}
 
     impl State for PendingReview {
         fn request_review(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            Box::new(Published {})
+        }
+    }
+
+    struct Published {}
+
+    impl State for Published {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+
+        fn approve(self: Box<Self>) -> Box<dyn State> {
             self
         }
     }
