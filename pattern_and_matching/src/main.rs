e269c25d966155fd908057225dd5f30d139b8b3c
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-07-17 00:02:20 +0900

    Update main.rs

--- pattern_and_matching/src/main.rs
+++ pattern_and_matching/src/main.rs
@@ -29,7 +29,7 @@ fn main() {
 
     let v = vec!['a', 'b', 'c'];
 
-    for(index, value) in v.iter().enumerate() {
+    for (index, value) in v.iter().enumerate() {
         println!("{} is at index {}", value, index);
     }
 
@@ -39,4 +39,61 @@ fn main() {
 
     let point = (3, 5);
     print_coodinates(&point);
+
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = 5;
+
+    match x {
+        1..=5 => println!("ont through fine"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    let p = Point { x: 0, y: 7 };
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+}
+
+struct Point {
+    x: i32,
+    y: i32,
 }
