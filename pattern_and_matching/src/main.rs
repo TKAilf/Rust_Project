22507a97dc3156f70dd711586b9f11d0af90562b
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-07-17 17:30:29 +0900

    Update main.rs

--- pattern_and_matching/src/main.rs
+++ pattern_and_matching/src/main.rs
@@ -91,9 +91,61 @@ fn main() {
     let Point { x, y } = p;
     assert_eq!(0, x);
     assert_eq!(7, y);
+
+    let p = Point { x: 0, y: 7 };
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+
+    let _quit = Message::Quit;
+    let _move = Message::Move { x: 0, y: 0 };
+    let _write = Message::Write(String::from("hoge"));
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y);
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+    }
+
+    let points = vec![
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 5 },
+        Point { x: 10, y: -3 },
+    ];
+
+    let sum_of_squares: i32 = points.iter().map(|&Point { x, y }| x * x + y * y ).sum();
+    println!("sum_of_squares: {}", sum_of_squares);
+
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+    println!("{:?}", ((feet, inches), Point { x, y }));
+
+    fn foo(_: i32, y: i32) {
+        println!("This code only uses the y parameter: {}", y);
+    }
+
+    foo(3, 4);
 }
 
+#[derive(Debug)]
 struct Point {
     x: i32,
     y: i32,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
