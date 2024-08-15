593c188d552b0c3c734a1a24b1c3815188b124d5
k.toshiki <ktoshiki0511@yahoo.co.jp>
2024-08-15 21:58:48 +0900

    Create single_thread_web_server

--- create_multi_thread_web_server/hello/src/main.rs
+++ create_multi_thread_web_server/hello/src/main.rs
@@ -1,5 +1,6 @@
-use std::net::TcpListener;
+use std::fs::File;
 use std::io::prelude::*;
+use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
@@ -14,8 +15,48 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    // if buffer.starts_with(get) {
+    //     let mut file = File::open("hello.html").unwrap();
+
+    //     let mut contents = String::new();
+    //     file.read_to_string(&mut contents).unwrap();
+
+    //     let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    //     stream.write(response.as_bytes()).unwrap();
+    //     stream.flush().unwrap();
+
+    //     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    // } else {
+    //     let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+    //     let mut file = File::open("404.html").unwrap();
+    //     let mut contents = String::new();
+
+    //     file.read_to_string(&mut contents).unwrap();
+
+    //     let response = format!("{}{}", status_line, contents);
+
+    //     stream.write(response.as_bytes()).unwrap();
+    //     stream.flush().unwrap();
+    // }
 }
